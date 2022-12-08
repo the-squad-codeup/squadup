95cf6a984a7d6780c79398f86b6bf8705080e3f5
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-12-08 16:25:51 -0600

    added getterssetters for username

--- src/main/java/pro/squadup/models/User.java
+++ src/main/java/pro/squadup/models/User.java
@@ -27,6 +27,14 @@ public class User {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -59,9 +67,9 @@ public class User {
 
     public User (User copy) {
         this.id = copy.id;
+        this.username = copy.username;
         this.email = copy.email;
         this.password = copy.password;
     }
-
 
 }
