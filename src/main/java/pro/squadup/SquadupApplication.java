5afa95a46cec717bc66fdd71f7b3334d37633516
Stephen Pflug <stephen@pflug.us>
2022-12-08 14:30:08 -0600

    first commit

---
+++ src/main/java/pro/squadup/SquadupApplication.java
@@ -0,0 +1,13 @@
+package pro.squadup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadupApplication.class, args);
+    }
+
+}
