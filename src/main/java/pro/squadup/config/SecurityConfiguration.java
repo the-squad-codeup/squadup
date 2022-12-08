a93f372d434a1dbb63ec93e9bd7550cffe3f2844
Stephen Pflug <stephen@pflug.us>
2022-12-08 15:06:33 -0600

    updated pom.xml file to change to spring boot 2.7.5

--- src/main/java/pro/squadup/config/SecurityConfiguration.java
+++ src/main/java/pro/squadup/config/SecurityConfiguration.java
@@ -1,4 +1,55 @@
 package pro.squadup.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Login configuration
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
+                .permitAll()
+                // Logout configuration
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout")
+                // Pages only viewable when logged in
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/movie/list/*",
+                        "/movies",
+                        "/movie/list",
+                        "/review/user/view"
+                )
+                .authenticated()
+                // Pages viewable without logging in
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/",
+                        "/index",
+                        "/reviews",
+                        "/review/all/view",
+                        "/pwreset"
+                )
+                .permitAll()
+        ;
+        return http.build();
+    }
+
 }
