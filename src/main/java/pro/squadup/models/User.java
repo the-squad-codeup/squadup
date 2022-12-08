f772c71008a9739430129fde61e90652b13de6c6
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-12-08 16:04:26 -0600

    user model is complete without relationships

--- src/main/java/pro/squadup/models/User.java
+++ src/main/java/pro/squadup/models/User.java
@@ -1,4 +1,67 @@
 package pro.squadup.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private  String username;
+
+    @Column(nullable = false, length = 75, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User (User copy) {
+        this.id = copy.id;
+        this.email = copy.email;
+        this.password = copy.password;
+    }
+
+
 }
