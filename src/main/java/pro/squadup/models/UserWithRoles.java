c39c6ac7e3640957c95fcb38e613bb1b757e6f63
Stephen Pflug <stephen@pflug.us>
2022-12-08 15:51:58 -0600

    added UserWithRoles model

---
+++ src/main/java/pro/squadup/models/UserWithRoles.java
@@ -0,0 +1,35 @@
+package pro.squadup.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserWithRoles extends User implements UserDetails {
+    public UserWithRoles(User user) { super(user); }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
