ded536bf9670f7f6c8418e079354172497912bc3
Stephen Pflug <stephen@pflug.us>
2022-12-09 19:33:00 -0600

    added LanguageRepository

---
+++ src/main/java/pro/squadup/repositories/LanguageRepository.java
@@ -0,0 +1,10 @@
+package pro.squadup.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.squadup.models.Language;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+    Language findById(Long id);
+}
