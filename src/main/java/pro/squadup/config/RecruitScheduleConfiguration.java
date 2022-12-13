553e2d55f34fc0164c68eeb413e6e0151ff8ae19
Stephen Pflug <stephen@pflug.us>
2022-12-13 10:53:50 -0600

    optimized imports for recruit schedule config

--- src/main/java/pro/squadup/config/RecruitScheduleConfiguration.java
+++ src/main/java/pro/squadup/config/RecruitScheduleConfiguration.java
@@ -4,18 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import pro.squadup.models.*;
-import pro.squadup.repositories.ComradeRepository;
-import pro.squadup.repositories.GenreRepository;
-import pro.squadup.repositories.RecruitRepository;
-import pro.squadup.repositories.UserRepository;
 import pro.squadup.services.RecruitMatchingService;
-
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Configuration
 @EnableScheduling
