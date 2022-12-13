006159c22a6ddcdda898d759caf692bd51c60c6d
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-12-13 15:54:22 -0600

    new controllers/models/repos completed

---
+++ src/main/java/pro/squadup/repositories/LocationRepository.java
@@ -0,0 +1,9 @@
+package pro.squadup.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.squadup.models.Location;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
+}
