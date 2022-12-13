fdb7b6652d40fd1477ced29197bdd9b800b92356
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-12-13 15:18:14 -0600

    adding more controllers for functionality

---
+++ src/main/java/pro/squadup/controllers/PlatformController.java
@@ -0,0 +1,24 @@
+package pro.squadup.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.squadup.models.Platform;
+import pro.squadup.repositories.PlatformRepository;
+
+import java.util.List;
+
+@RestController
+public class PlatformController {
+
+    private PlatformRepository platformDao;
+
+    public PlatformController(PlatformRepository platformDao) {
+        this.platformDao = platformDao;
+    }
+
+    @GetMapping("/platform/all")
+    public List<Platform> getAllPlatforms(){
+        return platformDao.findAll();
+    }
+
+}
---
+++ src/main/java/pro/squadup/controllers/RatingController.java
@@ -0,0 +1,4 @@
+package pro.squadup.controllers;
+
+public class RatingController {
+}
