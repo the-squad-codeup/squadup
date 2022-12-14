5e77a9d8945f4b47c2b18e35ee1b26992a693ca7
Stephen Pflug <stephen@pflug.us>
2022-12-14 09:59:33 -0600

    added methods to preferences.js to print correct dropdowns

--- src/main/resources/static/js/preferences.js
+++ src/main/resources/static/js/preferences.js
@@ -51,10 +51,14 @@ $(function() {
             let user = await Fetch.Get.currentUser().then(res => res);
             this.locationSelectElement(user);
             this.languageSelectElement(user);
+            this.gameRatingSelectElement(user);
+            this.platformSelectElement(user);
         },
         async locationSelectElement(user) {
             let locations = await Fetch.Get.all("location").then(res => res);
-            let $locations = $("#location");
+            console.log("All locations get results:");
+            console.log(locations);
+            let $locations = $("#locations");
             $locations.empty();
             for(let location in locations) {
                 //check if location matches user location
@@ -63,9 +67,32 @@ $(function() {
         },
         async languageSelectElement(user) {
             let languages = await Fetch.Get.all("language").then(res => res);
-            let $languages = $("#language");
+            console.log("All languages get results:");
+            console.log(languages);
+            let $languages = $("#languages");
             for(let language in languages) {
-                // check if any
+                // check if language matches user language
+                // append option element with "selected" if match
+            }
+        },
+        async gameRatingSelectElement(user) {
+            let gameRatings = await Fetch.Get.all("rating").then(res => res);
+            console.log("All ratings get results:");
+            console.log(gameRatings);
+            let $gameRatings = $("#game-ratings");
+            for(let gameRating in gameRatings) {
+                //check if gameRating matches user gameRating
+                // append option element with "selected" if match
+            }
+        },
+        async platformSelectElement(user) {
+            let platforms = await Fetch.Get.all("platform").then(res => res);
+            console.log("All platforms get results:");
+            console.log(platforms);
+            let $platforms = $("#platforms");
+            for(let platform in platforms) {
+                //check if platform matches user platform
+                //append option element with "selected" if match
             }
         }
     }
