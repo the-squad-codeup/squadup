b735c095556230ee0a27339b112f10be4010caf8
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-12-14 11:05:28 -0600

    added autowired to controllers

--- src/main/java/pro/squadup/controllers/SocialController.java
+++ src/main/java/pro/squadup/controllers/SocialController.java
@@ -1,18 +1,26 @@
 package pro.squadup.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pro.squadup.services.UrlService;
 
 
 @Controller
 public class SocialController {
 
+    @Autowired
+    private UrlService url;
+
     @GetMapping("/comrades")
-    public String comradesPage(){
+    public String comradesPage(Model model){
+        model.addAttribute("url", url);
         return "social/comrades";
     }
 
     @GetMapping("/recruits")
-    public String recruitsPage(){
+    public String recruitsPage(Model model){
+        model.addAttribute("url", url);
         return "social/recruits";
     }
 
