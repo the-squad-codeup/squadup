433cc9c401e5e4c27bf381e61369beb5359fc9f2
Stephen Pflug <stephen@pflug.us>
2022-12-15 10:37:28 -0600

    added event listeners to get user text input when search button clicked

--- src/main/resources/static/js/games.js
+++ src/main/resources/static/js/games.js
@@ -1,3 +1,29 @@
 $(function () {
     console.log("Inside games.js");
+
+    const Games = {
+        initialize() {
+            console.log("inside Games.initialize()");
+            Events.initialize();
+        }
+    }
+
+    const Events = {
+        initialize() {
+            console.log("Inside Events.initialize()");
+            $(document)
+                .on("click", "#game-search-button", function() {
+                    console.log("button has been clicked. Value in input: ");
+                    console.log($("#game-search-input").val());
+                })
+                .on("keyup", function(e) {
+                    if($("#game-search-input").is(":focus") && e.key === "Enter") {
+                        $("#game-search-button").trigger("click");
+                    }
+                })
+            ;
+        }
+    }
+
+    Games.initialize();
 });
