8f222182a5c0973cec17d78eb4b85315aa998502
Aiden Luce <luceaiden4124@gmail.com>
2022-12-15 14:06:23 -0600

    carousel getting close

--- src/main/resources/static/js/splash.js
+++ src/main/resources/static/js/splash.js
@@ -3,35 +3,31 @@ let animating = false;
 
 window.setInterval(function(){
     startAnimations();
-}, 4000);
+}, 800);
 
 function startAnimations() {
     if(!animating){
         animating = true;
-        $('.item--'+items[0]).css({ zIndex: 1 });
-        $('.item--'+items[1]).css({ zIndex: 3 });
-        $('.item--'+items[2]).css({ zIndex: 3 });
-        $('.item--'+items[3]).css({ zIndex: 0 });
-        $('.item--'+items[4]).css({ zIndex: 0 });
+        $('.item--'+items[0]).css({ zIndex: -5 });
+        $('.item--'+items[1]).css({ zIndex: -4 });
+        $('.item--'+items[2]).css({ zIndex: -3 });
+        $('.item--'+items[3]).css({ zIndex: -2 });
+        $('.item--'+items[4]).css({ zIndex: -1 });
 
         $('.item--'+items[0]).animate({
-            left: '100px',
-            width: '150px',
-            height: '100px'
+            left: '70vw',
         }, 1000);
         $('.item--'+items[1]).animate({
-            left: '325px',
+            left: '55vw',
         }, 1000);
         $('.item--'+items[2]).animate({
-            left: '550px'
+            left: '40vw'
         }, 1000);
         $('.item--'+items[3]).animate({
-            left: '450px',
-            width: '90px',
-            height: '60px'
+            left: '25vw'
         }, 1000);
         $('.item--'+items[4]).animate({
-            left: '250px'
+            left: '10vw'
         }, 1000);
         console.log('animations-complete');
         setTimeout(function(){
