a9b6cf456e068e3743f2421bebee382ae78fcd97
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-12-16 13:53:02 -0600

    working on recruits functionality

--- src/main/java/pro/squadup/repositories/RecruitRepository.java
+++ src/main/java/pro/squadup/repositories/RecruitRepository.java
@@ -12,4 +12,6 @@ public interface RecruitRepository extends JpaRepository<Recruit, Long> {
     boolean existsByUserOneAndUserTwo(User userOne, User userTwo);
 
     Set<Recruit> findAllByUserOne(User userOne);
+
+    Recruit findByUserOneAndUserTwo(User userOne, User userTwo);
 }
--- src/main/resources/static/js/recruits.js
+++ src/main/resources/static/js/recruits.js
@@ -1,14 +1,32 @@
 $(function() {
     console.log("Inside recruits.js");
-    // function printUserCards(user){
-    //     $("#card").html('');
-    // }
+    async function printUserCards(recruits) {
+        recruits = await recruits;
+        console.log(recruits);
+        $("#card").html('');
+        for (let recruit of recruits) {
+            $(`#card`).append(`
+                <div class="card mx-auto" data-recruit-id="${recruit.id}">
+                    <img class="card-img-top" src="https://i.imgur.com/0Z0Z0Z0.jpg" alt="Card image">
+                        <div class="card-body">
+                            <h4 class="card-title">${recruit.userTwo.username}</h4>
+                            <p class="card-text">${recruit.userTwo.username}'s Bio: ${recruit.userTwo.preferences.bio}</p>
+                            <a href="#" class="btn btn-primary squadup-link">SquadUp</a>
+                            <a href="#" class="btn btn-primary squaddown-link">SquadDown</a>
+                        </div>
+                </div>
+            `);
+        }
+    }
+
     async function getAllRecruits(){
         let results = await fetch("http://localhost:8080/recruits/all");
         let data = await results.json();
         console.log(data);
+        return data;
     }
 
-    getAllRecruits();
+    printUserCards(getAllRecruits());
 
 });
+
