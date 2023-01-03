f6d01d1879821bd8f6ec603827ce0c88635e3d77
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-01-03 16:05:43 -0600

    completing comrades controller

--- src/main/java/pro/squadup/controllers/ComradesController.java
+++ src/main/java/pro/squadup/controllers/ComradesController.java
@@ -1,26 +1,38 @@
 package pro.squadup.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.squadup.models.Comrade;
+import pro.squadup.models.User;
 import pro.squadup.repositories.ComradeRepository;
 import pro.squadup.repositories.RecruitRepository;
 import pro.squadup.repositories.UserRepository;
+import pro.squadup.utils.Utils;
+
+import java.util.Set;
 
 @RestController
 public class ComradesController {
 
     private UserRepository userDao;
 
-    private RecruitRepository recruitDao;
-
     private ComradeRepository comradeDao;
 
-    public ComradesController(UserRepository userDao, RecruitRepository recruitDao, ComradeRepository comradeDao) {
+    public ComradesController(UserRepository userDao, ComradeRepository comradeDao) {
         this.userDao = userDao;
-        this.recruitDao = recruitDao;
         this.comradeDao = comradeDao;
     }
 
+    @GetMapping("/comrades/all")
+    public Set<Comrade> getAllByUser(){
+        User user = userDao.findById(Utils.currentUserId()).get();
+        return user.getComrades();
+    }
+
+    @PostMapping("/")
+    public
 
 
 }
