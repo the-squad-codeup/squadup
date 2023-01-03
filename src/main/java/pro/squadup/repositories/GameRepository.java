f3fe174d0ce1a1fbd0046894f1f8e785d6776352
Aiden Luce <luceaiden4124@gmail.com>
2023-01-03 10:14:53 -0600

    user games

--- src/main/java/pro/squadup/repositories/GameRepository.java
+++ src/main/java/pro/squadup/repositories/GameRepository.java
@@ -3,9 +3,14 @@ package pro.squadup.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.squadup.models.Game;
+import pro.squadup.models.User;
+
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     boolean existsByIgdbId(Long igdbId);
     Game findByIgdbId(Long igdbId);
+
+    List<Game> findAllByUser(User user);
 }
