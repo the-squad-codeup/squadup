33b425f917687ca68e7275b9b01a100121db65a6
Aiden Luce <luceaiden4124@gmail.com>
2023-01-03 14:12:48 -0600

    appended

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -4,25 +4,14 @@ $(function (){
     async function getUserGames() {
         let userGames = await fetch(`${Utils.url()}game/user`).then(res => res.json());
         console.log(userGames)
+        for(let i = 0; i < userGames.length; i++){
+            $('.my-games').append(`
+                <div class="game">
+                    IM APPENDED!
+                </div>
+            `)
+        }
     }
     getUserGames()
 
-    async function getGame(){
-        let game = await fetch(`${Utils.url()}game/user`).then(res => res.json());
-        function renderUserGames(data){
-            console.log("inside renderUserGames");
-            for(let i = 0; i < data.list.length -1; i++){
-                console.log(data)
-                let game = data.list[i]
-                $('.gamesContainer').append(`
-                        <div class="m-4 col-2" >
-                            <div class="card">
-                                IM APPENDED!
-                            </div>
-                        </div>
-                    `)
-            }
-        }
-    }
-    getGame()
 })
