1fd818ddd24048184583c887d2bc90b1c4a8884a
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-01-04 13:38:30 -0600

    fixed append issues per stephen

--- src/main/resources/static/js/comrades.js
+++ src/main/resources/static/js/comrades.js
@@ -11,24 +11,27 @@ $(function() {
         $("#card").html('');
         for (let recruit of recruits) {
             $(`#card`).append(`
-                <div class=" card h-100 row-cols-1" data-recruit-id="${recruit.id}">
-                <img class="card-img-top" src="https://i.imgur.com/0Z0Z0Z0.jpg" alt="user profile picture">
-                        <div class="card-body">
-                            <h4 class="card-title">${recruit.userTwo.username}</h4>
-                            <p class="card-text">${recruit.userTwo.username}'s Bio: ${recruit.userTwo.preferences.bio}</p>
-                `);
-            for (let userTwoGame of recruit.userTwo.userTwoGames) {
-                $(`#card .card[data-recruit-id="${recruit.id}"] .card-body div`).append(`
-                <div>
-                    <img class="card-img-top" src="${userTwogame.game.image}" alt="${userTwogame.game.name} icon">
-                </div>
-                    <div>
-                        <a href="#"  class="btn btn-primary unfriend-link">Remove Recruit</a>
+                 <div class="card h-100 row-cols-3" data-recruit-id="${recruit.id}">
+                    <img class="card-img-top" src="https://i.imgur.com/0Z0Z0Z0.jpg" alt="user profile picture">
+                    <div class="card-body">
+                        <h4 class="card-title">${recruit.userTwo.username}</h4>
+                        <p class="card-text">${recruit.userTwo.username}'s Bio: ${recruit.userTwo.preferences.bio}</p>
                     </div>
+                    <div>
+                        <a href="#" className="btn btn-primary unfriend-link">Remove Recruit</a>
+                    </div>
+                </div>
             `);
+            for (let userTwoGame of recruit.userTwo.preferences.games) {
+                $(`#card`).children(`[data-recruit-id="${recruit.id}"]`).children(".card-body").append(`
+                    <div>
+                        <img class="card-img-top" src="${userTwoGame.artwork}" alt="${userTwoGame.title} icon">
+                    </div>
+                `);
             }
         }
     }
+
 
 
     document.getElementById("card").addEventListener('click', async function(e){
