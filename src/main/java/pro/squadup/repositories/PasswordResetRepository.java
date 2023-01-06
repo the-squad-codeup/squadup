6b5be3ec3244ea1f904814397a28fa200a1c0ab1
Stephen Pflug <stephen@pflug.us>
2023-01-06 08:11:08 -0600

    added PasswordResetRepository and PasswordResetController

---
+++ src/main/java/pro/squadup/repositories/PasswordResetRepository.java
@@ -0,0 +1,10 @@
+package pro.squadup.repositories;
+
+import pro.squadup.models.PasswordReset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetRepository extends JpaRepository<PasswordReset, Long> {
+    PasswordReset findByToken(String token);
+}
