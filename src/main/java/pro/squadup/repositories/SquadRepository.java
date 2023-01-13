121f860c041a2f0399bceb3fbce1c6e4eefc3f82
Stephen Pflug <stephen@pflug.us>
2023-01-13 09:37:53 -0600

    created hq controller and method in user controller

--- src/main/java/pro/squadup/repositories/SquadRepository.java
+++ src/main/java/pro/squadup/repositories/SquadRepository.java
@@ -10,4 +10,5 @@ import java.util.Set;
 @Repository
 public interface SquadRepository extends JpaRepository<Squad, Long> {
     Set<Squad> findAllByMembers(User members);
+    Set<Squad> findAllByOwner(User owner);
 }
