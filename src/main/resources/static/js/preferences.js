e2c8bbe49b62fa71d946ef79540c997ad8a6210d
Stephen Pflug <stephen@pflug.us>
2023-01-29 10:01:37 -0600

    commenting/cleaning code

--- src/main/resources/static/js/preferences.js
+++ src/main/resources/static/js/preferences.js
@@ -3,11 +3,14 @@ import { Overlays } from "./pref-overlays.js";
 
 $(function() {
 
+    // MyPreferences object with globals and methods
     const MyPreferences = {
+        // Initializes event handlers and prints preferences form
         initialize() {
             Events.initialize();
             Print.form();
         },
+        // returns true if array includes the passed in language
         arrayIncludesLanguage(array, language) {
             for(let e of array) {
                 if(e.language === language.language) {
@@ -16,6 +19,7 @@ $(function() {
             }
             return false;
         },
+        // returns true if array includes the passed in platform
         arrayIncludesPlatform(array, platform) {
             for(let e of array) {
                 if(e.type === platform.type) {
@@ -24,6 +28,7 @@ $(function() {
             }
             return false;
         },
+        // finds all selected language options and adds them to a correctly formatted options array then returns it
         packageLanguageOptions: function ($div) {
             let options = [];
             for(let child of $div.children()) {
@@ -31,9 +36,9 @@ $(function() {
                     options.push({language: child.value});
                 }
             }
-
             return options;
         },
+        // finds all selected platform options and adds them to a correctly formatted options array then returns it
         packagePlatformOptions($div) {
             let options = [];
             for(let child of $div.children()) {
@@ -43,6 +48,8 @@ $(function() {
             }
             return options;
         },
+        // returns true if the form contains all required fields
+        // as it is checking, it runs methods to update view if field is not correctly filled out
         formValidated(preferencesObject) {
             let isLocationValid = this.locationValidated(preferencesObject.location);
             let areLanguagesValid = this.languagesValidated(preferencesObject.languages);
@@ -63,6 +70,8 @@ $(function() {
             Print.formNotValid();
             return false;
         },
+        // returns true if validated
+        // changes field background in view to reflect this
         locationValidated(location) {
             let isValid = true;
             if(location.timezone === "" || location.timezone === "Unspecified Time Zone") {
@@ -71,6 +80,8 @@ $(function() {
             Print.locationValidation(isValid);
             return isValid;
         },
+        // returns true if validated
+        // changes field background in view to reflect this
         languagesValidated(languages) {
             let isValid = true;
             if(languages.length < 1) {
@@ -79,6 +90,8 @@ $(function() {
             Print.languagesValidation(isValid);
             return isValid;
         },
+        // returns true if validated
+        // changes field background in view to reflect this
         ratingValidated(rating) {
             let isValid = true;
             if(rating.rating === "") {
@@ -87,6 +100,8 @@ $(function() {
             Print.ratingValidation(isValid);
             return isValid;
         },
+        // returns true if validated
+        // changes field background in view to reflect this
         platformsValidated(platforms) {
             let isValid = true;
             if(platforms.length < 1) {
@@ -95,6 +110,8 @@ $(function() {
             Print.platformsValidation(isValid);
             return isValid;
         },
+        // returns true if validated
+        // changes field background in view to reflect this
         gamertagValidated(gamertag) {
             let isValid = true;
             if(gamertag === "" || !this.gamertagValidDiscordName(gamertag)) {
@@ -103,6 +120,7 @@ $(function() {
             Print.gamertagValidation(isValid);
             return isValid;
         },
+        // checks to see if gamertag is in the correct discord format as follows: discordname#1234
         gamertagValidDiscordName(gamertag) {
             let gamertagSplit = gamertag.split("#");
             if(gamertagSplit.length !== 2 || isNaN(gamertagSplit[1]) || gamertagSplit[1].length !== 4) {
@@ -110,6 +128,8 @@ $(function() {
             }
             return true;
         },
+        // returns true if validated
+        // changes field background in view to reflect this
         bioValidated(bio) {
             let isValid = true;
             if(bio === "") {
@@ -118,6 +138,7 @@ $(function() {
             Print.bioValidation(isValid);
             return isValid;
         },
+        // packages object to be sent in post fetch request in correct format to match POJO Preferences
         packagePreferencesObject() {
             const preferencesObject = {
                 bio: $("#bio").val(),
@@ -144,20 +165,26 @@ $(function() {
         baseUrl: $("#base-url").text()
     };
 
+    // Fetch object with methods
     const Fetch = {
+        // Get methods
         Get: {
+            // returns all preferences of specific type (ie languages, platforms, etc)
             async all(type) {
                 let res = await fetch(`${Utils.url()}${type}/all`).then(res => res);
                 let data = await res.json();
                 return data;
             },
+            // returns current user data
             async currentUser() {
                 let res = await fetch(`${Utils.url()}user/get`).then(res => res);
                 let data = await res.json();
                 return data;
             }
         },
+        // Post methods
         Post: {
+            // passes preferences object to be set for current user
             async updatedPreferences(preferencesObject) {
                 const postOptions = {
                     method: 'POST',
@@ -172,7 +199,10 @@ $(function() {
         }
     }
 
+    // Print object and methods
     const Print = {
+        // Prints form with correct preferences fields filled out
+        // based off current user Preferences stored in database
         async form() {
             let user = await Fetch.Get.currentUser().then(res => res);
             await this.locationSelectElement(user);
@@ -183,17 +213,24 @@ $(function() {
             await this.gamertagElement(user);
             await this.bioElement(user);
         },
+        // prints mature language checkbox based on if column in table is true or false
         async matureLanguageCheckboxElement(user){
             if(user.preferences.matureLanguage) {
                 $("#mature-language").attr("checked", "checked");
             }
         },
+        // prints user field
+        // pulled from preferences saved in database
         async gamertagElement(user) {
             $("#gamertag").val(`${user.preferences.gamertag}`);
         },
+        // prints biography field
+        // pulled from preferences saved in database
         async bioElement(user) {
             $("#bio").val(`${user.preferences.bio}`);
         },
+        // prints correct location options
+        // pulled from preferences saved in database
         async locationSelectElement(user) {
             let locations = await Fetch.Get.all("location").then(res => res);
             let $locations = $("#location");
@@ -211,8 +248,11 @@ $(function() {
                     `);
                 }
             }
+            // calls toggleOverlay to show which section of map photo is highlighted
             Overlays.toggleOverlay();
         },
+        // prints correct languages
+        // pulled from preferences saved in database
         async languageSelectElement(user) {
             let languages = await Fetch.Get.all("language").then(res => res);
             let $languages = $("#languages");
@@ -230,6 +270,8 @@ $(function() {
                 }
             }
         },
+        // prints correct age rating
+        // pulled from preferences saved in database
         async gameRatingSelectElement(user) {
             let gameRatings = await Fetch.Get.all("rating").then(res => res);
             let $gameRatings = $("#game-ratings");
@@ -247,6 +289,8 @@ $(function() {
                 }
             }
         },
+        // prints platforms
+        // pulled from preferences saved in database
         async platformSelectElement(user) {
             let platforms = await Fetch.Get.all("platform").then(res => res);
             let $platforms = $("#platforms");
@@ -264,30 +308,43 @@ $(function() {
                 }
             }
         },
+        // receives boolean and sets background
+        // red if false, normal if true
         locationValidation(isValid) {
             let locationInput = $("#select2-location-container");
             isValid ? locationInput.css("background", "") : locationInput.css("background", "#fc3503");
         },
+        // receives boolean and sets background
+        // red if false, normal if true
         languagesValidation(isValid) {
             let languagesInput = $("#languages").parent().find(".select2-selection");
             isValid ? languagesInput.css("background", "") : languagesInput.css("background", "#fc3503");
         },
+        // receives boolean and sets background
+        // red if false, normal if true
         ratingValidation(isValid) {
             let ratingInput = $("#select2-game-ratings-container");
             isValid ? ratingInput.css("background", "") : ratingInput.css("background", "#fc3503");
         },
+        // receives boolean and sets background
+        // red if false, normal if true
         platformsValidation(isValid) {
             let platformsInput = $("#platforms").parent().find(".select2-selection");
             isValid ? platformsInput.css("background", "") : platformsInput.css("background", "#fc3503");
         },
+        // receives boolean and sets background
+        // red if false, normal if true
         gamertagValidation(isValid) {
             let gamertagInput = $("#gamertag");
             isValid ? gamertagInput.css("background", "") : gamertagInput.css("background", "#fc3503");
         },
+        // receives boolean and sets background
+        // red if false, normal if true
         bioValidation(isValid) {
             let bioInput = $("#bio");
             isValid ? bioInput.css("background", "") : bioInput.css("background", "#fc3503");
         },
+        // adds text to form calling user to update form with correct values
         formNotValid() {
             $("#preferences-form").find("#validation-prepend").remove();
             $("#preferences-form").prepend(`
@@ -298,9 +355,15 @@ $(function() {
         }
     };
 
+    // Event handlers and methods
     const Events = {
+        // All event handlers
         initialize() {
             $(document)
+                // submits form to backend
+                // checks to see if all fields filled correctly first
+                // if true, submits form
+                // if false scrolls to top of page
                 .on("click", "#edit-preferences-submit-button", async function() {
                     let preferencesObject = MyPreferences.packagePreferencesObject();
                     if(MyPreferences.formValidated(preferencesObject)) {
@@ -310,10 +373,12 @@ $(function() {
                         document.body.scrollTop = document.documentElement.scrollTop = 0;
                     }
                 })
+                // changes map highlighted overlays when location element is changed
                 .on("change", "#location", function() {
                     Overlays.toggleOverlay();
                 })
             ;
+            // sets up select2 elements when page is loaded
             $(document).ready(function() {
                 $("#location").select2({
                     placeholder: "select a timezone",
@@ -332,7 +397,7 @@ $(function() {
         }
     }
 
+    // Initializes the whole JS script
     MyPreferences.initialize();
-
 
 });
