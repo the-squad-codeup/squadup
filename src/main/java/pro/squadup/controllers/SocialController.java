5d28d38b36cd531a27705ca1e9a638a8e1be2ac4
Stephen Pflug <stephen@pflug.us>
2023-01-30 11:15:57 -0600

    removed comrades and recruits views and controller methods

--- src/main/java/pro/squadup/controllers/SocialController.java
+++ src/main/java/pro/squadup/controllers/SocialController.java
@@ -7,15 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SocialController {
 
     // simple getmapping redirects
-    @GetMapping("/comrades")
-    public String comradesPage(){
-        return "social/comrades";
-    }
-
-    @GetMapping("/recruits")
-    public String recruitsPage(){
-        return "social/recruits";
-    }
 
     @GetMapping("/hq")
     public String socialHQPage() {
